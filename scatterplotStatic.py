10cca6011f490d61c654a6750714680ddaab5970
dipchakraborty-23 <dipchakraborty990@gmail.com>
2026-03-31 12:12:20 +0530

    Update scatterplotStatic.py

--- scatterplotStatic.py
+++ scatterplotStatic.py
@@ -1,13 +1,13 @@
 import matplotlib.pyplot as plt
 
-# Sample static data
+
 x = [1, 2, 3, 4, 5, 6]
 y = [10, 15, 13, 17, 20, 22]
 
-# Create scatter plot
+
 plt.scatter(x, y, color='green', marker='o')
 
-# Add title and labels
+
 plt.title("Simple Scatter Plot")
 plt.xlabel("X values")
 plt.ylabel("Y values")
