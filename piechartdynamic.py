d989166d0a7e6e24cbcbe8a244cb1fff307debe8
dipchakraborty-23 <dipchakraborty990@gmail.com>
2026-03-31 12:14:54 +0530

    Update piechartdynamic.py

--- piechartdynamic.py
+++ piechartdynamic.py
@@ -1,13 +1,12 @@
-
 import matplotlib.pyplot as plt
 
-# Number of categories
+
 n = int(input("Enter number of categories: "))
 
 labels = []
 sizes = []
 
-# Taking user input
+
 for i in range(n):
     label = input(f"Enter label for category {i+1}: ")
     value = float(input(f"Enter value for {label}: "))
@@ -15,11 +14,11 @@ for i in range(n):
     labels.append(label)
     sizes.append(value)
 
-# Create pie chart
+
 plt.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
 
-# Add title
+
 plt.title("Pie Chart (User Input)")
 
-# Display chart
+
 plt.show()
